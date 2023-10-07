cc15fe5985c9923e1a69da103c15370ff8928fbc
Love <honnylove77@gmail.com>
2023-10-07 11:59:04 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*	main - this is the entry point of the program
+*	Return: always zero
+*/
+	int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; ++a)
+	printf("a + '0'");
+	printf("\n");
+	return (0);
+}
