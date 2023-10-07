7c95e24f3d7b11191637a2d2bd6ef783d9ad2e91
Love <honnylove77@gmail.com>
2023-10-07 08:00:03 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+*       main - this is the main function
+*       Return: always zero
+*/
+
+	int main(void)
+{
+	char m;
+
+	for (m = 'a'; m <= 'z'; m++)
+	{
+	putchar(m);
+	}
+
+	for (m = 'A'; m <= 'Z'; m++)
+	{
+	putchar(m);
+	putchar('\n');
+	}
+	return (0);
+
+}
