f221c5e9dec04d9116a3c88d3f160314c829fe3a
Love <honnylove77@gmail.com>
2023-10-09 14:14:16 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,15 +7,15 @@
 int main(void)
 {
 	int i;
-	
+
 	for (i = 48; i <= 57; i++)
 	{
 	putchar(i);
 	if (i < 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+	{
+		putchar(',');
+		putchar(' ');
+	}
 	}
 	putchar('\n');
 
