0fe63557a5bd3c6ee7e053875ac33b6e15f9fd94
Love <honnylove77@gmail.com>
2023-10-23 17:13:02 -0700

    prefix substring length

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,4 +3,27 @@
 *	_strspn - gets the length of a prefix substring
 *	@s: function parameter 1
 *	@accept: function parameter 2
-*	Return: 
+*	Return:  the number of bytes in s
+*	which consist only of bytes from accept.
+*/
+	unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int index;
+
+	while (*s)
+	{
+	for (index = 0; accept[index]; index++)
+	{
+	if (*s == accept[index])
+	{
+	bytes++;
+	break;
+	}
+	else if (accept[index + 1] == '\0')
+	return (bytes);
+	}
+	s++;
+	}
+	return (bytes);
+}
