b4ebd6bf5e67fd1269c0f061349b89046f48632a
Love <honnylove77@gmail.com>
2023-11-10 18:16:21 -0800

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,14 +11,12 @@
 
 	va_start(list, n);
 
-	for (i = 0; i < n, i++)
+	for (i = 0; i < n; i++)
 	{
-		if (!separator)
-			printf("%d", va_arg(list, int));
-		else if (separator && i== 0)
-			printf("%d", va_arg(list, int));
-		else
-			printf("%s\d", separator, va_arg(list, int));
+		if (i > 0 && separator)
+			printf("%s", separator);
+
+			printf("%d", va_arg(list, int);
 	}
 	va_end(list);
 	printf("\n");
