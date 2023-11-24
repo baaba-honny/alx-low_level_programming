0a7c64b4490090c44a17d4a68332dd4e350e03de
Love <honnylove77@gmail.com>
2023-11-23 21:59:42 -0800

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,11 +6,11 @@
 *	@index: index
 *	Return: 1 if success
 */
-	int clear_bit(unsigned long int *n, unsigned int index);
+	int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(*n) * 8)
+	if (index >= sizeof(n) * 8)
 		return (-1);
 
-	*n &= ~(1UL << index);
+	*n &= ~(1 << index);
 	return (1);
 }
