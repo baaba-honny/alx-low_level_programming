49b85b90fd21f04ae12d07768e817e5fe1943978
Love <honnylove77@gmail.com>
2023-11-29 14:23:53 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,15 +13,34 @@
 	ssize_t _write;
 	char *buffer;
 
+	if (filename == NULL)
+		return (0);
+
 	_fd = open(filename, O_RDONLY);
 	if (_fd == -1)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
+	
+	if (buffer == NULL)
+	{
+		close(_fd);
+		return (0);
+	}
+
 	_read = read(_fd, buffer, letters);
+
+	if (_read == -1)
+	{
+		close(_fd);
+		return (0);
+	}
 	_write = write(STDOUT_FILENO, buffer, _read);
 
-	free(buffer);
+	if (_write == -1 || _read != _write)
+		return (0);
+
 	close(_fd);
+	free(buffer);
 	return (_write);
 }
